293eff76f8ae9c289a19466ad7048648fbe434b3
N-D-Duy <Ct060311@actvn.edu.vn>
2025-06-28 18:03:12 +0700

    chore: update domain url

--- assets/js/constant.js
+++ assets/js/constant.js
@@ -1,4 +1,4 @@
-export const HOST = "http://localhost:8080/api/v1";
-// export const HOST = "https://api.duynguyendev.xyz/api/v1";
+// export const HOST = "http://localhost:8080/api/v1";
+export const HOST = "https://api.ndwxdy.me/api/v1";
 export const DOCTOR_SCHEDULE_EXPORT = `${HOST}/core/schedule/export/`;
 export const APPOINTMENT_EXPORT = `${HOST}/core/appointments/export/`;
--- assets/js/notification.js
+++ assets/js/notification.js
@@ -13,7 +13,6 @@ $(document).ready(function () {
             return `${date.getFullYear()}-${pad(date.getMonth() + 1)}-${pad(date.getDate())}T${pad(date.getHours())}:${pad(date.getMinutes())}:${pad(date.getSeconds())}`;
         };
 
-
         let sendTime;
         if (isTest) {
             sendTime = new Date(new Date().getTime() + 10 * 1000);
